adc5a07ea468a3b2cd8f51f3c8e2118d7e2672b7
Ghislain1 <gzeleu@outlook.de>
2023-05-07 20:02:57 +0000

    CI Build Artifacts for Docs

---
+++ docs/985.e3c582429db91b04.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkghis=self.webpackChunkghis||[]).push([[985],{6985:(E,l,u)=>{u.r(l),u.d(l,{AboutModule:()=>d});var s=u(4755),t=u(4466),n=u(2223);let d=(()=>{class o{}return o.\u0275fac=function(M){return new(M||o)},o.\u0275mod=n.oAB({type:o}),o.\u0275inj=n.cJS({imports:[s.ez,t.m]}),o})()}}]);
