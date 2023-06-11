a5c71fd519d06452749303732ce4923dbd04bb3a
Ghislain1 <gzeleu@outlook.de>
2023-06-11 16:46:38 +0200

    redeisgn UI mit tailwaindcss

--- ghis/src/app/works/works.module.ts
+++ ghis/src/app/works/works.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { WorksComponent } from './works.component';
+import { RouterModule, Routes } from '@angular/router';
+import { SharedModule } from '../shared/shared.module';
 
+const routes: Routes = [{ path: '', component: WorksComponent }]
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    WorksComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(routes),
+    SharedModule,
   ]
 })
 export class WorksModule { }
--- ghis/tailwind.config.js
+++ ghis/tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    screens:{
+        sm:'480px',
+        md:'768px',
+        lg:'976px',
+        xl:'1440px'
+    },
+    extend: {
+      colors:{
+        ghisRed:'hsl(12,88%,59%)',
+        ghisDarkRed:'hsl(1,99%,99%)'
+
+      }
+    },
   },
   plugins: [],
 }
