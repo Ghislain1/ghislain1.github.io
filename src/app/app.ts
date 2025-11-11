78485d9468d3e7fc1d6e70490cd90fea07b18965
Ghislain Z. <gzeleu@outlook.de>
2025-11-11 16:38:02 +0100

    Refactor code structure for improved readability and maintainability

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,24 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { Profile } from './profile/profile';
+import { Footer } from './footer/footer';
+import { Home } from './home/home';
+import { Experience } from './experience/experience';
+import { Connect } from './connect/connect';
+import { Education } from './education/education';
+import { Technology } from './technology/technology';
+import { SideNav } from './side-nav/side-nav';
+import { Header } from './header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    SideNav,
+    Footer
+  ],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrls: ['./app.scss']
 })
 export class App {
   protected readonly title = signal('ghis');
---
+++ src/app/connect/connect.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-connect',
+  imports: [],
+  templateUrl: './connect.html',
+  styleUrl: './connect.scss',
+})
+export class Connect {
+
+}
